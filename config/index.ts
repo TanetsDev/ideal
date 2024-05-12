5dc6abd4c77cea44878895a1653d4649c417031b
Tanets Olexandr <alextanetsdev@gmail.com>
2024-05-13 00:37:56 +0300

    fix env

--- config/index.ts
+++ config/index.ts
@@ -5,8 +5,8 @@ const sanity = {
 };
 
 const google = {
-  CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
-  CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
+  CLIENT_SECRET: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
   API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY || "",
 };
 
